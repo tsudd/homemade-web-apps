558c728cb274a8eacce4d8dab40bad4705db061f
tsudd <alex.kr1stehko@gmail.com>
2020-07-23 13:13:26 +0300

    pathetic try to use bem methology

---
+++ ttt-webpack-bootstrap-bem/pages/app.js
@@ -0,0 +1,156 @@
+"use strict"
+
+function Cell(elem) {
+  this.content = 'o';
+  this.unfree = false;
+  this.cell = elem;
+}
+
+let application = function () {
+let turn = 1;
+let emptyCellsAmount = 9;
+let gameIsRunning = true;
+let cells = [];
+const ticClass = 'table__cell_tic';
+const tacClass = 'table__cell_tac';
+let info = document.getElementsByClassName('turn-header');
+let player = document.getElementsByClassName('turn');
+$(player).addClass(ticClass);
+let elmnts = document.getElementsByClassName('table__cell');
+$(info).html('Turn');
+for (let i = 0, k = 0; i < 3; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++, k++) {
+        row.push(new Cell(elmnts[k]));
+        $(elmnts[k]).html(k + 1);
+    }
+    cells.push(row);
+}
+
+let checkMethod = function checkCellsForWinner() {
+    for (let i = 1, j = 0; j < 3; j++) {
+        let str = '';
+        str += cells[i - 1][j].content;
+        str += cells[i][j].content;
+        str += cells[i + 1][j].content;
+        if (showWinner(str)) {
+            return;
+        }
+        if (j === 1) {
+            str = '';
+            str += cells[j][j].content;
+            str += cells[j - 1][j - 1].content;
+            str += cells[j + 1][j + 1].content;
+            if (showWinner(str)) {
+            return;
+            }
+            str = '';
+            str += cells[j][j].content;
+            str += cells[j + 1][j - 1].content;
+            str += cells[j - 1][j + 1].content;
+            if (showWinner(str)) {
+            return;
+            }
+        }
+        str = '';
+        str += cells[j][i - 1].content;
+        str += cells[j][i].content;
+        str += cells[j][i + 1].content;
+        if (showWinner(str)) {
+            return;
+        }
+    }
+
+} 
+
+let showWinner = function show(str) {
+    let ans = '', win = '';
+    if (emptyCellsAmount == 0) {
+        ans = 'DRAW';
+    }
+    if (str === 'XXX') {
+        ans = 'win';
+        win = 'X';
+    } else if (str === 'OOO') {
+        ans = 'win';
+        win = 'O';
+    }
+    if (ans) {
+        $(player).removeClass([ticClass, tacClass]);
+        $(player).addClass((win) ? ((win === 'X') ? ticClass : tacClass) : '');
+        $(info).html(ans);
+        gameIsRunning = false;
+        return true;
+    }
+    return false;
+}
+
+let restart = function restartGame() {
+    turn = 1;
+    gameIsRunning = true;
+    emptyCellsAmount = 9;
+    $(info).html('Turn');
+    $(player).addClass(ticClass);
+    let elements = document.getElementsByClassName('table__cell');
+    for (let i = 0, k = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++, k++) {
+            cells[i][j].unfree = false;
+            cells[i][j].content = 'o';
+            $(elements[k]).removeClass([ticClass, tacClass]);
+        }   
+    }
+}
+
+return {
+    getTurn : (() => {
+        return turn;
+    }),
+    getGameStatus : (() => {
+        return gameIsRunning;
+    }),
+    check : checkMethod,
+    restart : restart,
+    getCell : function (id) {
+        let i = 1, j, k = id;
+
+        if (k > 3) {
+            while (k > 3) {
+                k -= 3;
+                i += 1;
+            }
+            j = k;
+        } else {
+            j = k;
+        }
+        return cells[i - 1][j - 1];
+    },
+    fillCell : (() => {
+        return (turn) ? ticClass : tacClass;
+    }),
+    endTurn : function () {
+        turn = turn ? 0 : 1;
+        emptyCellsAmount--;
+        $(player).removeClass([ticClass, tacClass]);
+        $(player).addClass((turn) ? ticClass : tacClass);
+    },
+};
+}();
+
+$('.table__cell').click(function () {
+    if (!application.getGameStatus()) {
+        return;
+    }
+    let cell = application.getCell($(this).html());
+    if (cell.unfree) {
+        return;
+    }
+    cell.unfree = true;
+    cell.content = application.getTurn() ? 'X' : 'O';
+    $(this).addClass(application.fillCell());
+    application.endTurn();
+    application.check();
+})
+
+$('.restart-btn').click(function () {
+    application.restart();
+})
